cc433e9d408f76378918fa1c25d1620b3247a486
Blake Macnair <blakemacnair@gmail.com>
2019-02-25 15:50:42 -0600

    Fix some init formatting and remove unused stuff from the main executable.

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 #!flask/bin/python
 from flask import Flask
 from flask_restful import Api
-from flask_restful import reqparse
-from routes.GET.helloworld import HelloWorld
 from routes.POST.payload import GithubWebhookPayload
 
 
 app = Flask(__name__)
 api = Api(app)
-
-api.add_resource(HelloWorld, '/')
 
 api.add_resource(GithubWebhookPayload, '/payload')
 
--- routes/POST/X_GitHub_Events/pullrequest.py
+++ routes/POST/X_GitHub_Events/pullrequest.py
@@ -1,10 +1,10 @@
 from flask_restful import Resource, reqparse
 
+
 class PullRequest:
 
-    __init__(self, args):
+    def __init__(self, args):
         self.args = args
-
 
     def handle_request(self):
         pass
