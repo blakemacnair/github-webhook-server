ce26cf77dd4122f79dc0bce9db9bd57f7c74fc47
Blake Macnair <blakemacnair@gmail.com>
2019-02-25 14:10:01 -0600

    Add github headers for expected webhooks.

--- routes/POST/payload.py
+++ routes/POST/payload.py
@@ -1,10 +1,15 @@
 from flask_restful import Resource, reqparse
 
 parser = reqparse.RequestParser()
-parser.add_argument('action')
+
+parser.add_argument('action', location='json')
+parser.add_argument('X-GitHub-Event', type=str, location='headers')
+parser.add_argument('X-Hub-Signature', type=str, location='headers')
+parser.add_argument('X-GitHub-Delivery', type=str, location='headers')
 
 class GithubWebhookPayload(Resource):
     def post(self):
         args = parser.parse_args()
+        print(args)
         action = {'action': args['action']}
         return action, 201
