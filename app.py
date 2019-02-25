7ba652641b79c057965573f2aace8ec5cefeb957
Blake Macnair <blakemacnair@gmail.com>
2019-02-25 13:08:07 -0600

    Add a parser and an endpoint to point github webhooks to.

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 #!flask/bin/python
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_restful import reqparse
+
+parser = reqparse.RequestParser()
+parser.add_argument('action')
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,6 +14,14 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
+
+class GithubWebhookPayload(Resource):
+    def post(self):
+        args = parser.parse_args()
+        action = {'action': args['action']}
+        return action, 201
+
+api.add_resource(GithubWebhookPayload, '/payload')
 
 if __name__ == '__main__':
     app.run(debug=True)
